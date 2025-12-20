410ea16f498d4e1c53bec64a1c624e2dae65cd44
Jabed Islam <125015472+developer-jabed@users.noreply.github.com>
2025-12-20 19:44:44 +0600

    write script with gpt suggestion

--- src/app/helper/stripe.ts
+++ src/app/helper/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from "stripe";
 import config from "../../config";
 
 export const stripe = new Stripe(config.stripeSecretKey as string, {
-  apiVersion: "2025-09-30.clover",
+  apiVersion: "2025-10-29.clover",
 });
